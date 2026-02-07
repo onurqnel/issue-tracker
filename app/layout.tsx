c728c68396bb6be78f3a4987be49ac52172f59cc
Onur <onur.onel2230@gmail.com>
2026-02-07 18:23:43 -0500

    Customize Radix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,14 @@ import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./Navbar";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Inter } from "next/font/google";
+import "./theme-config.css";
 
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Issue Tracking Dashboard",
@@ -15,11 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body>
-        <Theme>
+        <Theme accentColor="purple" radius="large">
           <Navbar />
           <main className="p-5">{children}</main>
+          <ThemePanel />
         </Theme>
       </body>
     </html>
